bc98fe0d510ea66662b3f3909bac3bae9efff7ef
HLT@HIT(SZ) <51396729+HLT-HITSZ@users.noreply.github.com>
2021-11-29 16:17:21 +0800

    Update losses.py
    updated

--- losses.py
+++ losses.py
@@ -1,7 +1,3 @@
-"""
-Author: Yonglong Tian (yonglong@mit.edu)
-Date: May 07, 2020
-"""
 from __future__ import print_function
 
 import torch
@@ -18,8 +14,7 @@ def normalization(data):
 
 
 class SupConLoss(nn.Module):
-    """Supervised Contrastive Learning: https://arxiv.org/pdf/2004.11362.pdf.
-    It also supports the unsupervised contrastive loss in SimCLR"""
+    
     def __init__(self, temperature=0.07, contrast_mode='one',
                  base_temperature=0.07):
         super(SupConLoss, self).__init__()
@@ -28,18 +23,7 @@ class SupConLoss(nn.Module):
         self.base_temperature = base_temperature
 
     def forward(self, features, labels=None, mask=None):
-        """Compute loss for model. If both `labels` and `mask` are None,
-        it degenerates to SimCLR unsupervised loss:
-        https://arxiv.org/pdf/2002.05709.pdf
-
-        Args:
-            features: hidden vector of shape [bsz, n_views, ...].
-            labels: ground truth of shape [bsz].
-            mask: contrastive mask of shape [bsz, bsz], mask_{i,j}=1 if sample j
-                has the same class as sample i. Can be asymmetric.
-        Returns:
-            A loss scalar.
-        """
+        
         device = (torch.device('cuda')
                   if features.is_cuda
                   else torch.device('cpu'))
@@ -50,7 +34,7 @@ class SupConLoss(nn.Module):
         if len(features.shape) > 2:
             features = features.view(features.shape[0], features.shape[1], -1)
 
-        # get batch_size
+        # Get batch_size
         batch_size = features.shape[0]
 
         if labels is not None and mask is not None:
@@ -75,20 +59,19 @@ class SupConLoss(nn.Module):
             anchor_count = 1
         elif self.contrast_mode == 'all':
             anchor_feature = contrast_feature
-            # ?对比数目
+            # Set contrast count
             anchor_count = contrast_count
         else:
             raise ValueError('Unknown mode: {}'.format(self.contrast_mode))
 
-        # compute logits
+        # Compute logits
         anchor_dot_contrast = torch.div(
             torch.matmul(anchor_feature, contrast_feature.T),
             self.temperature)
 
-        # for numerical stability
-        # 数值稳定性
+        # For numerical stability
         logits_max, _ = torch.max(anchor_dot_contrast, dim=1, keepdim=True)
-        # 获得logits最大值
+        # Get logits
         logits = anchor_dot_contrast - logits_max.detach()
 
         logits_min, _ = torch.min(logits, dim=1, keepdim=True)
@@ -96,28 +79,28 @@ class SupConLoss(nn.Module):
         _range = logits_max - logits_min
         logits = torch.div(logits-logits_min,_range)
 
-        # tile mask
+        # Tile mask
         mask = mask.repeat(anchor_count, contrast_count)
         # print("mask",mask)  # 16*16
 
-        # mask-out self-contrast cases
+        # Mask-out self-contrast cases
         logits_mask = torch.scatter(
-            torch.ones_like(mask), #返回一个填充了标量值1的张量
+            torch.ones_like(mask), # Returns a tensor filled with the value 1
             1,
             torch.arange(batch_size * anchor_count).view(-1, 1).to(device),
             0
         )
-        # print("logits_mask",logits_mask)  #16*16 对角线
+        # print("logits_mask",logits_mask)  #16*16
         mask = mask * logits_mask
 
         exp_logits = torch.exp(logits) * logits_mask
 
         log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
 
-        # compute mean of log-likelihood over positive
+        # Compute mean of log-likelihood over positive
         mean_log_prob_pos = (mask * log_prob).sum(1) / (mask.sum(1)+1)
 
-        # loss
+        # Get loss
         loss = - (self.temperature / self.base_temperature) * mean_log_prob_pos
         loss = loss.view(anchor_count, batch_size).mean()
 
